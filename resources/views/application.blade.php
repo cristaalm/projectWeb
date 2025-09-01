58844ace4dfda6d4abb216a96969d1da880499c3
Eduarduar <eduardoarcega1997@gmail.com>
2025-09-01 12:10:48 -0600

    fix: Corrección del nombre y logo

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -3,9 +3,9 @@
 
 <head>
   <meta charset="UTF-8" />
-  <link rel="icon" href="{{ asset('uni-logo.ico') }}" />
+  <link rel="icon" href="{{ asset('favicon.ico') }}" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>UNIAMA 2</title>
+  <title>RENOVA</title>
   <link rel="stylesheet" type="text/css" href="{{ asset('loader.css') }}" />
   @vite(['resources/js/main.js'])
 </head>
