5674eabed78cae21908c8bc43dd2daac73319cc2
Eduarduar <eduardoarcega1997@gmail.com>
2025-09-29 13:51:40 -0600

    fix: corrección en el orden de migraciones y seeders

--- database/seeders/ScanSeeder.php
+++ database/seeders/ScanSeeder.php
@@ -10,16 +10,6 @@ class ScanSeeder extends Seeder
 {
     public function run(): void
     {
-        // Scan::factory(200)->create();
-        Scan::factory(5000)
-        ->state([
-            'user_id' => 2,
-            'scan_status' => ScanStatus::SUCCESS->value,
-            'is_valid' => fake()->randomElement([
-                true,
-                false,
-            ]),
-        ])
-        ->create();
+        Scan::factory(2000)->create();
     }
 }
